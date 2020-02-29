80638014cb3cfd3e9e39c5cca8997306ea78ea6b
Antonio Sucre <ajs2104@gmail.com>
2020-02-29 09:27:33 -0300

    Linq notacao similar SQL

--- ExLinq/Program.cs
+++ ExLinq/Program.cs
@@ -36,26 +36,46 @@ namespace ExLinq
                 new Product(){Id=11,Name="Level",Price=70.0,Category=c1}
             };
 
+            //LAMBDA Expressions
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            var r1 = from p in products
+                     where p.Category.Tier == 1 && p.Price < 900
+                     select p;
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
             Print("TIER 1 AND PRICE < 900.0: ", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products
+                     where p.Category.Name == "Tools"
+                     select p.Name;
             Print("NAMES OF CATEGORY TOOLS: ", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 = from p in products
+                     where p.Name[0] == 'C'
+                     select new { p.Name, p.Price, CategoryName = p.Category.Name };
             Print("PRODUCT NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT: ", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products
+                     where p.Category.Tier == 1
+                     orderby p.Price, p.Name
+                     select p;
             Print("TIER 1 ORDER BY PRICE THEN NAME: ", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5=  (from p in r4
+                     select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN NAME SKIP 2 AND TAKE 4: ", r5);
 
-            var r6 = products.First();
+            //var r6 = products.First();
+            var r6 = (from p in products select p).First();
             Console.WriteLine("FIRST: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            var r7 = (from p in products
+                      where p.Price > 3000
+                      select p).FirstOrDefault();
             Console.WriteLine("FIRST OR DEFAULT: " + r7);
             Console.WriteLine();
 
@@ -86,7 +106,9 @@ namespace ExLinq
             Console.WriteLine("CATEGORY 1 AGREGATES SUM: " + r15);
             Console.WriteLine();
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products
+                      group p by p.Category;                    
             foreach(IGrouping<Category,Product> group in r16)
             {
                 Console.WriteLine("CATEGORY " + group.Key.Name + ":");
